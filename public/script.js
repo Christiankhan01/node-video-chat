877a972ac51cf2d48da9ca25fa3bd2bee4d0b863
Christian Khan <christiankhan@Hotmail.com>
2020-12-07 15:17:23 -0500

    new user setup

--- public/script.js
+++ public/script.js
@@ -1,26 +1,45 @@
+
+
 //Access Media - video and audio
-const socket = io('/'); 
-const videoGrid = document.getElementById('video-grid'); 
-const myVideo = document.createElement('video'); 
-myVideo.muted = true; 
+
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '9015'
+});
 
 //global variable declation for YOUR video stream 
 let myVideoStream
+
 navigator.mediaDevices.getUserMedia({
-    video: true, 
-    audio: true 
+    video: true,
+    audio: true
 }).then(stream => {
-    myVideoStream = stream; 
+    myVideoStream = stream;
     addVideoStream(myVideo, stream)
 })
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+//listen on 'user-connected' for new user
+socket.on('user-connected', (userId) => {
+    connectToNewUser(userId);
+})
 
-socket.emit('join-room'); 
+const connectToNewUser = (userId) => {
+    console.log("user --> "+ userId + " <-- has joined the room");
+}
 
 const addVideoStream = (video, stream) => {
-    video.srcObject = stream; 
+    video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
-        video.play() 
+        video.play()
     })
-    videoGrid.append(video); 
+    videoGrid.append(video);
 }
 
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 const server = require('http').Server(app); 
 const io = require('socket.io')(server); 
 const {v4: uuidv4} = require('uuid'); 
+const { ExpressPeerServer } = require('peer'); 
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+}); 
 
 app.set('view engine', 'ejs'); 
 app.use(express.static('public')); 
 
 
-
+app.use('/peerjs', peerServer); 
 
 app.get('/', (req, res) => {
     //RoomID
@@ -22,9 +26,11 @@ app.get('/:room', (req, res) => {
 })
 
 io.on('connection', socket => {
-    //user joins room here - emit accepted
-    socket.on('join-room', () => {
-        console.log("Joined Room"); 
+    //user joins room here - emit accepted - 'userId' has connected
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId); 
+        //broadcast to room that user has connected 
+        socket.to(roomId).broadcast.emit('user-connected', userId); 
     })
 })
 
